c7fcb761fb1e23fda57290410d14be8b11245051
Shane Richards <shane.richards@fticonsulting.com>
2020-04-22 01:18:32 -0400

    added alert to indicate the result of the login attempt; #2

--- OnTheMap/Controllers/LoginViewController.swift
+++ OnTheMap/Controllers/LoginViewController.swift
@@ -24,9 +24,51 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginUsingCredentials(_ sender: Any) {
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        let credentials = UdacityCredentials(udacity: Credentials(username: email, password: password))
         
+        let url = URL(string: "https://onthemap-api.udacity.com/v1/session")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // handle this properly
+        request.httpBody = try! JSONEncoder().encode(credentials)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    self.displayAlert(title: "Request Error", message: "\(error)")
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                do {
+                    let range = 5..<data.count
+                    let data = data.subdata(in: range)
+                    
+                    let decoder = JSONDecoder()
+                    let loginResponse = try decoder.decode(LoginResponse.self, from: data)
+                    let sessionId = loginResponse.session.id
+                    
+                    self.displayAlert(title: "Login Successful", message: "Session ID: \(sessionId ?? "No ID")")
+                } catch { error
+                    self.displayAlert(title: "Error", message: "Unable to parse response \(error)")
+                }
+            }
+        }
+        
+        task.resume()
     }
     
     @IBAction func loginUsingFacebook(_ sender: Any) {
+    }
+    
+    func displayAlert(title: String, message: String) -> Void {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        self.present(alert, animated: true, completion: nil)
     }
 }
