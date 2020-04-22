0d1c6bf537bdb645807a7505f58391d3424a38d7
Shane Richards <shane.richards@fticonsulting.com>
2020-04-21 22:50:41 -0400

    connected actions and outlets; #2

--- OnTheMap/Controllers/LoginViewController.swift
+++ OnTheMap/Controllers/LoginViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
+
+
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var facebookButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginUsingCredentials(_ sender: Any) {
+        
+    }
+    
+    @IBAction func loginUsingFacebook(_ sender: Any) {
     }
 }
