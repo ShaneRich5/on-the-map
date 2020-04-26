cd5bef6a88cdc08c70f6ff8483edcc7c6e8e72aa
Shane Richards <shane.richards@fticonsulting.com>
2020-04-25 23:37:39 -0400

    reattached views controllers to new screens; #9

---
+++ OnTheMap/Controllers/AddLocationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddLocationViewController.swift
+//  OnTheMap
+//
+//  Created by Shane Richards on 4/25/20.
+//  Copyright © 2020 Shane Richards. All rights reserved.
+//
+
+import UIKit
+
+class AddLocationViewController: UIViewController {
+    
+    static var segueIdentifier = "addPin"
+    
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var addPinButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addPinButton.isEnabled = false
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == AddLocationViewController.segueIdentifier {
+            
+        }
+    }
+}
--- OnTheMap/Controllers/AddPinViewController.swift
+++ OnTheMap/Controllers/AddPinViewController.swift
@@ -2,7 +2,7 @@
 //  AddPinViewController.swift
 //  OnTheMap
 //
-//  Created by Shane Richards on 4/22/20.
+//  Created by Shane Richards on 4/25/20.
 //  Copyright © 2020 Shane Richards. All rights reserved.
 //
 
@@ -10,16 +10,21 @@ import UIKit
 
 class AddPinViewController: UIViewController {
 
-    @IBOutlet weak var cancelButton: UIBarButtonItem!
-    @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var locationTextField: UITextField!
-    @IBOutlet weak var findLocationButton: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
     
-    @IBAction func cancelSubmission(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
+    */
+
 }
