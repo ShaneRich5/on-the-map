7484dc2e8b1fc2ad88d6e6b927ed109f395be07c
Shane Richards <shane.richards@fticonsulting.com>
2020-05-02 17:35:46 -0400

    cleaning up code; #12

--- OnTheMap/Controllers/AddPinViewController.swift
+++ OnTheMap/Controllers/AddPinViewController.swift
@@ -13,6 +13,7 @@ class AddPinViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var finishButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var location: String!
     var mediaUrl: String!
@@ -29,17 +30,24 @@ class AddPinViewController: UIViewController {
         return try? encoder.encode(studentLocation)
     }
     
+    func showLoadingState(isLoading: Bool) {
+        if isLoading {
+            activityIndicator.startAnimating()
+        } else {
+            activityIndicator.stopAnimating()
+        }
+        
+        finishButton.isEnabled = !isLoading
+    }
+    
     @IBAction func saveStudentLocation(_ sender: Any) {
         guard let url = URL(string: "https://onthemap-api.udacity.com/v1/StudentLocation"), let requestBody = buildRequestBody() else {
             self.displayDefaultAlert(title: "Error!", message: "Unable to save your location at this time.")
             return
         }
         
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = requestBody
-
+        let request = buildRequest(url: url, data: requestBody)
+        
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data, error == nil else {
                 self.displayDefaultAlert(title: "Save Failed!", message: error?.localizedDescription ?? "Error unclear")
@@ -53,5 +61,15 @@ class AddPinViewController: UIViewController {
             }
         }
         task.resume()
+    }
+    
+    func buildRequest(url: URL, data: Data) -> URLRequest {
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = data
+        
+        return request
     }
 }
